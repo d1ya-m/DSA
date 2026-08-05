370564dd5f976fb31b7315a7ec5d33e0acdc96ac
Diya Moogi <119802749+d1ya-m@users.noreply.github.com>
2026-08-05 18:56:52 +0530

    Time: 2398 ms (10.13%), Space: 19.4 MB (18.08%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        left=0
+        right=len(s1)-1
+        d={}
+        for i in range (len(s1)):
+            d[s1[i]]=d.get(s1[i], 0)+1
+
+        while(right<len(s2) and left<=len(s2)-len(s1)):
+
+        
+            while(right<len(s2) and left<len(s2)-len(s1) and s2[left] not in s1):
+                left+=1
+                right+=1
+            d1={}
+            for i in range(left, right+1):
+                d1[s2[i]]=d1.get(s2[i], 0)+1
+            if (d1==d):
+                return True
+            left+=1
+            right+=1
+        return False
+
+
+            
+        
+        
